8f515ce29147d7094b42b0b3c1ad13e3a848858c
N.Fujita <noppys2012@gmail.com>
2012-10-13 04:20:47 +0900

    Add .gitignore file. Add uplogd.h file.

---
+++ src/uplogd.h
@@ -0,0 +1,11 @@
+/* Uplogd
+ * Copyright(C) 2012 N.Fujita All rights reserved.
+ *
+ * -File path for uplogd
+ */
+
+
+/* File path */
+#define LOGFILE       "/tmp/uplog.log"
+#define PIDFILE       "/var/run/uplogd.pid"
+
