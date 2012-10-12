48c409aa50b4fa05d38e97c18f9751f78de95a35
N.Fujita <noppys2012@gmail.com>
2012-10-13 04:44:38 +0900

    uplogdのファイルパス定義をuplogd.hへ移動

--- src/uplog_common.h
+++ src/uplog_common.h
@@ -1,13 +1,10 @@
 /* Uplogger
  * Copyright(C) 2012 N.Fujita All rights reserved.
  *
- * - File path and Client&Server setting
+ * - Common settings for the client(uplogger) and the server(uplogd)
  */
 
-
 /* File path */
-#define LOGFILE       "/tmp/uplog.log"
-#define PIDFILE       "/var/run/uplogd.pid"
 #define SOCKET_FILE   "/var/run/uplog.sock"
 
 /* Message length for UNIX domain */
